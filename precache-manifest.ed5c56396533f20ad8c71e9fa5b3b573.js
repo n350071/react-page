a4e364d184ea90dda1fc16fd45adb4230f19d58a
nao0515ki <ishigaki0515naoki@gmail.com>
2019-04-17 04:39:18 +0900

    Updates

---
+++ precache-manifest.ed5c56396533f20ad8c71e9fa5b3b573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c1c17707debafef04205",
+    "url": "/static/js/main.19877562.chunk.js"
+  },
+  {
+    "revision": "360682d07611ddead705",
+    "url": "/static/js/2.e0524695.chunk.js"
+  },
+  {
+    "revision": "c1c17707debafef04205",
+    "url": "/static/css/main.167e81bf.chunk.css"
+  },
+  {
+    "revision": "7842c90bc72f530e64cee69020d28a27",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nao0515ki.github.io/precache-manifest.5930abb580f641b9462c188dd3496af6.js"
+  "/precache-manifest.ed5c56396533f20ad8c71e9fa5b3b573.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nao0515ki.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
